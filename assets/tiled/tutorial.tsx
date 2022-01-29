d4b350630b2050b8b1761d0f034228735f2dd112
Briland Mathieu <briland.mathieu@gmail.com>
2022-01-29 20:27:00 +0100

    moult scene

---
+++ assets/tiled/tutorial.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tutorial" tilewidth="16" tileheight="16" tilecount="704" columns="32">
+ <image source="../backgrounds/tutorial.png" width="512" height="352"/>
+</tileset>
